e387b67be0a4480d07c64e7a65fa0b566921708b
Michael Quinn <mikejquinn@gmail.com>
2014-02-26 21:28:02 -0500

    Add basic scaffolding for projects and bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BugTracker::Application.routes.draw do
+  resources :bugs
+
+
+  resources :projects
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
